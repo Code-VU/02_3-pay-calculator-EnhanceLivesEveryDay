82ddeda0c4e46f1fd49351503a21046e573e01ea
EnhanceLivesEveryDay <124171045+EnhanceLivesEveryDay@users.noreply.github.com>
2023-04-22 07:53:34 -0500

    Add files via upload

--- area-calc.py
+++ area-calc.py
@@ -2,4 +2,14 @@
 # Sample Output:
 # r = 1.1
 # Area = 3.8013271108436504
+# Use the following formula to calculate circle area: A = πr². 
 
+my_pi = 3.141592653589793
+my_radius = float(input("What is the radius of the circle: "))
+my_radius = my_radius*my_radius
+
+my_area = my_pi * my_radius
+
+print (my_area)
+
+# end assignment
