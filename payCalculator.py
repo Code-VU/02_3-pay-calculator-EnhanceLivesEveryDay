62de89a8aeccc78e07889e16b0a106c45edfb638
EnhanceLivesEveryDay <hardestyhouse65202c@gmail.com>
2023-04-21 22:09:48 -0500

    Second Commit

---
+++ payCalculator.py
@@ -0,0 +1,19 @@
+def calculatePay():
+    # Implement your solution in between the two comment blocks
+    print("calculating pay")
+    # This first line is provided for you
+    hours = float(input("Enter Hours: "))
+    rate = float(input("Enter Rate: "))
+    gross_pay = hours * rate
+
+    print (gross_pay)
+
+    # end assignment
+
+
+## If you want to test locally before you try to sync
+## Open your terminal and run > python payCalculator.py
+
+#Ignore this for now. 
+if __name__ == "__main__":
+    calculatePay()
