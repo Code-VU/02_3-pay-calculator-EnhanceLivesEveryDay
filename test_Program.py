07bd0af154f48bd39907c33c6921e54a2234c82a
EnhanceLivesEveryDay <hardestyhouse65202c@gmail.com>
2023-04-22 09:26:28 -0500

    Will this work

--- test_Program.py
+++ test_Program.py
@@ -1,28 +1,16 @@
-# Assignment
-Write a program to prompt the user for hours and rate per hour to compute gross pay.
+import io
+from random import randint
+import random
+from unittest.mock import Mock
+import payCalculator
 
-## Formulas
-Gross Pay = Hours * Rate
+def test_payCalculator_prints_correct_result(capfd, monkeypatch):
+    rate = float(randint(1, 100))
+    hours = randint(1, 100)
+    inp = [rate, hours]
+    monkeypatch.setattr('builtins.input', lambda _:inp.pop())
+    payCalculator.calculatePay()
 
-## Starter code
-```python
-# This first line is provided for you
-
-hrs = input("Enter Hours:")
-```
-
-## Desired Output
-Please note, your gross pay should be printed by itself on its own line.
-
-```python
-Enter Hours: 35
-Enter Rate: 2.75
-96.25
-```
-
-## Testing
-1. Open your terminal, make sure it's pointed at the right files
-2. Type in `pytest` and hit enter to see if your code passes!
-
-## Note:
-Please pay attention to types... if you try to multiply 2 strings, you will probably get an error.  You may need to `cast` your input to be a number type.
+    out, err = capfd.readouterr()
+    expected = 'calculating pay\n{}\n'.format(rate * hours)
+    assert out == expected
